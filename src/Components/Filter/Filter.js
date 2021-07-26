f898bc809e026f346217f261d0a1a544a6e22de6
Maria Golubeva <mitsunari8ishida@gmail.com>
2021-07-26 14:41:47 +0300

    Upload HW

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import shortid from "shortid";
+import PropTypes from "prop-types";
+import styles from "./styles.module.css";
+
+class Filter extends Component {
+    static defaultProps = {
+        value: "",
+    };
+
+    static propTypes = {
+        onChange: PropTypes.func.isRequired,
+        value: PropTypes.string,
+    };
+    render() {
+        const inputId = shortid.generate();
+        const { onChange, value } = this.props;
+        return (
+            <label className={styles.filterLabel} htmlFor={inputId}>
+                Search
+                <input
+                    id={inputId}
+                    className={styles.filterInput}
+                    type="text"
+                    name="filter"
+                    value={value}
+                    onChange={onChange}
+                />
+            </label>
+        );
+    }
+}
+
+export default Filter;
